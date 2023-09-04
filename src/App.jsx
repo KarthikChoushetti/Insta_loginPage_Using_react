18a5c01d63254f8b0a8605500236b2b098c231d6
KarthikChoushetti <122105290+KarthikChoushetti@users.noreply.github.com>
2023-09-04 13:44:34 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Input from './Components.jsx'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const increase=()=>{
+    setCount((count)=>count+1)
+  }
+  const decrease=()=>{
+    setCount((count)=>count-1)
+  }
   return (
     <>
-   
-     <Input/>
+     <div className='container'>
+      <h1>{count}</h1>
+    <div className='btn'>
+      <button onClick={increase} className='incr'>Increase</button>
+      <button onClick={decrease} className='decr'>Decrease</button>
+    </div>
+     </div>
     </>
   )
 }
